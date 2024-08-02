12ae6902302fe336e4c0a6a1ce78671bc55daf74
user <user@192.168.219.102>
2024-08-03 00:30:28 +0900

    2024/08/02 spring boot 게시판만들기

---
+++ portfolio/src/main/java/com/spring/portfolio/controller/BoardController.java
@@ -0,0 +1,22 @@
+package com.spring.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class BoardController {
+
+	@GetMapping("/")
+	public String mainBoard() {
+		return "mainBoard";
+	}
+	
+	@GetMapping("/board/write")
+	public String boardWrite() {
+		return "boardWrite";
+	}
+	
+}
